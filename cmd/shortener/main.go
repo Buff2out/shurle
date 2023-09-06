0340b88956a0853afe0000dad4fd367cf3faf0c3
Rockython <61116928+Rockython@users.noreply.github.com>
2023-09-06 19:50:57 +0300

    iter1 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	pg "github.com/Buff2out/shurle/internal/app/transport/pages"
+	"net/http"
+)
+
+func main() {
+	//fmt.Println("222ffwuef")
+	//shello.PrintHello()
+
+	//links := make(map[string]string)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(`/`, pg.HandleShurlPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
