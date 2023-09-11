e3f437b64ae8864c8603e29b2e4c213889e2ab5e
Rockython <61116928+Rockython@users.noreply.github.com>
2023-09-11 21:27:57 +0300

    Надеюсь вернул всё. Небезопасно правда. И мог упустить что, но посмотрим...

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	cfgs "github.com/Buff2out/shurle/internal/app/config/server"
+	"github.com/Buff2out/shurle/internal/app/transport/ginsetrout"
+)
+
+func main() {
+	////fmt.Println("222ffwuef")
+	////shello.PrintHello()
+	//
+	////links := make(map[string]string)
+	//mux := http.NewServeMux()
+	//
+	//mux.HandleFunc(`/`, pg.HandleShurlPage)
+	//
+	//err := http.ListenAndServe(`:8080`, mux)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	serverConfig := cfgs.GetServerConfigFromFlags()
+	//fmt.Println(serverConfig)
+	r := ginsetrout.SetupRouter(serverConfig.P)
+	// Listen and Server in 0.0.0.0:8080
+	err := r.Run(serverConfig.S)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		package main
+
+		import (
+		    "fmt"
+		    "net/http"
+
+		    "github.com/gin-gonic/gin"
+		)
+
+		func main() {
+		    r := gin.Default()
+		    r.GET("/foo", func(c *gin.Context) {
+		        c.String(http.StatusOK, "bar")
+		        fmt.Println(c.Request.Host+c.Request.URL.String())
+		    })
+		    // Listen and Server in 0.0.0.0:8080
+		    r.Run(":8080")
+		}
+	*/
+}
---
+++ internal/app/transport/pages/pages.go
@@ -0,0 +1,51 @@
+package pages
+
+import (
+	"fmt"
+	shserv "github.com/Buff2out/shurle/internal/app/services/shurlserv"
+	"io"
+	"net/http"
+	"strings"
+)
+
+var links = make(map[string]string)
+
+func HandleShurlPage(res http.ResponseWriter, req *http.Request) {
+	//res.Header().Set("Access-Control-Allow-Origin", "*")
+	if http.MethodPost == req.Method { // Добавить ещё условие проверки длинности
+
+		res.Header().Set("content-type", "text/plain") // wow, http.ContentTypeText doesn't work
+
+		res.WriteHeader(http.StatusCreated) // 201
+		hash := shserv.EvaluateHashAndReturn()
+
+		b, err := io.ReadAll(req.Body)
+		if err != nil {
+			panic(err)
+		}
+		links[hash] = string(b)
+		_, err = res.Write([]byte(fmt.Sprintf(`%s:%s%s%s%s%s`, `http`, `/`, `/`, `localhost:8080`, `/`, hash)))
+		if err != nil {
+			return
+		}
+	} else if http.MethodGet == req.Method {
+		//fmt.Println("gsdf")
+		res.Header().Set("content-type", "text/plain") // wow, http.ContentTypeText doesn't work
+		hash := strings.Split(req.URL.Path, "/")[1]
+		res.Header().Set("Location", (fmt.Sprintf(links[hash])))
+		res.WriteHeader(http.StatusTemporaryRedirect) // 307
+		//_, err := res.Write([]byte(fmt.Sprintf(`http://%s`, links[hash])))
+		//if err != nil {
+		//	return
+		//}
+	} else {
+		res.WriteHeader(http.StatusBadRequest) // 201
+	}
+}
+
+//func GetLinkByHashAndRedirect(res http.ResponseWriter, req *http.Request) {
+//	res.Header().Set("Access-Control-Allow-Origin", "*")
+//	res.Header().Set("content-type", "text/plain")
+//	res.WriteHeader(http.StatusTemporaryRedirect) // 307
+//
+//}
