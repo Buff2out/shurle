8a76aa3d50d439b26971d071983f0685de19e149
Rockython <61116928+Rockython@users.noreply.github.com>
2023-09-27 19:10:10 +0300

    затёр комменты в main go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,20 +6,6 @@ import (
 )
 
 func main() {
-	////fmt.Println("222ffwuef")
-	////shello.PrintHello()
-	//
-	////links := make(map[string]string)
-	//mux := http.NewServeMux()
-	//
-	//mux.HandleFunc(`/`, pg.HandleShurlPage)
-	//
-	//err := http.ListenAndServe(`:8080`, mux)
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//envs.IsEnvsSet()
 
 	/* TODO
 	1 - Навести порядочек в "контроллерах"
@@ -36,25 +22,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	/*
-		package main
-
-		import (
-		    "fmt"
-		    "net/http"
-
-		    "github.com/gin-gonic/gin"
-		)
-
-		func main() {
-		    r := gin.Default()
-		    r.GET("/foo", func(c *gin.Context) {
-		        c.String(http.StatusOK, "bar")
-		        fmt.Println(c.Request.Host+c.Request.URL.String())
-		    })
-		    // Listen and Server in 0.0.0.0:8080
-		    r.Run(":8080")
-		}
-	*/
 }
