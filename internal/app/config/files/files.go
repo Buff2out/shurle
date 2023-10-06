8636c59dfbe2caa6d36792eb129d7522378c738a
Rockython <61116928+Rockython@users.noreply.github.com>
2023-10-06 19:12:58 +0300

    дописал паттерны работы с файлами.

--- internal/app/config/files/files.go
+++ internal/app/config/files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"bufio"
 	"encoding/json"
 	Event "github.com/Buff2out/shurle/internal/app/api/shortener"
 	"os"
@@ -8,40 +9,57 @@ import (
 
 type Producer struct {
 	file *os.File // Файл для записи
+	// добавляем Writer в Producer
+	writer *bufio.Writer
 }
 
 type Consumer struct {
 	file *os.File // Файл для чтения
+	// заменяем Reader на Scanner
+	scanner *bufio.Scanner
 }
 
 func NewProducer(filename string) (*Producer, error) {
-	// открываем файл для записи в конец
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Producer{file: file}, nil
-}
-
-func (p *Producer) Close() error {
-	// закрываем файл
-	return p.file.Close()
+	return &Producer{
+		file: file,
+		// создаём новый Writer
+		writer: bufio.NewWriter(file),
+	}, nil
 }
 
 func NewConsumer(filename string) (*Consumer, error) {
-	// открываем файл для чтения
 	file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Consumer{file: file}, nil
+	return &Consumer{
+		file: file,
+		// создаём новый scanner
+		scanner: bufio.NewScanner(file),
+	}, nil
 }
 
-func (c *Consumer) Close() error {
-	// закрываем файл
-	return c.file.Close()
+func (c *Consumer) ReadEvent() (*Event.ShURLFile, error) {
+	// одиночное сканирование до следующей строки
+	if !c.scanner.Scan() {
+		return nil, c.scanner.Err()
+	}
+	// читаем данные из scanner
+	data := c.scanner.Bytes()
+
+	event := Event.ShURLFile{}
+	err := json.Unmarshal(data, &event)
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
 }
 
 func (p *Producer) WriteEvent(event *Event.ShURLFile) error {
@@ -49,9 +67,27 @@ func (p *Producer) WriteEvent(event *Event.ShURLFile) error {
 	if err != nil {
 		return err
 	}
-	// добавляем перенос строки
-	data = append(data, '\n')
 
-	_, err = p.file.Write(data)
-	return err
+	// записываем событие в буфер
+	if _, err := p.writer.Write(data); err != nil {
+		return err
+	}
+
+	// добавляем перенос строки
+	if err := p.writer.WriteByte('\n'); err != nil {
+		return err
+	}
+
+	// записываем буфер в файл
+	return p.writer.Flush()
+}
+
+func (p *Producer) Close() error {
+	// закрываем файл
+	return p.file.Close()
+}
+
+func (c *Consumer) Close() error {
+	// закрываем файл
+	return c.file.Close()
 }
